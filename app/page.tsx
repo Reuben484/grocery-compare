a1ce9ee61a8ef4256048a454dd1e1ab8f76b00b8
Reuben484 <reuharding@gmail.com>
2026-05-08 23:48:09 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">
+        Grocery Compare
+      </h1>
+
+      <p className="mt-4">
+        Compare supermarket prices near you.
+      </p>
+    </main>
+  );
+}
